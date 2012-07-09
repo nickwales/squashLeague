d0cac4dc32719bf233aec690b30ab9ad984d5ea0
Nick Wales <nwales@homeaway.com>
2012-07-09 13:20:43 -0500

    Setup mailing configuration.
    Set domain and mail host.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,7 @@ SquashLeague::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+  
+  # Email setup
+  config.action_mailer.default_url_options = { :host => 'squash.wimbledonclub.co.uk' }
 end
