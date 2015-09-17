92c707608c575182f83b12b199cac57feebd4084
Nick Wales <nwales@homeaway.com>
2015-09-17 08:43:47 -0500

    added vagrant machine details to gitignore, vagrantfile and deploy changes

---
+++ README.md
@@ -0,0 +1,11 @@
+Squash League Manager
+=====================
+
+A bespoke rails application for the Wimbledon squash club leagues. 
+
+
+Create digitalocean droplet
+---------------------------
+
+    vagrant up --provider=digital_ocean
+
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -15,8 +15,8 @@ set :stage, :staging
 # definition into the server list. The second argument
 # something that quacks like a hash can be used to set
 # extended properties on the server.
+#server '104.131.114.79', user: 'root', roles: %w{app} #, my_property: :my_value
 server '46.101.42.167', user: 'root', roles: %w{app} #, my_property: :my_value
-
 # you can set custom ssh options
 # it's possible to pass any option but you need to keep in mind that net/ssh understand limited list of options
 # you can see them in [net/ssh documentation](http://net-ssh.github.io/net-ssh/classes/Net/SSH.html#method-c-start)
